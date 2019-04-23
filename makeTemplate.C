1f82351caf573e00887cc5c98994b9335087c8dd
Andre Frankenthal <afrankenthal@gmail.com>
2019-04-23 00:55:58 -0400

    Cleaned structure of makeTemplate.C

--- makeTemplate.C
+++ makeTemplate.C
@@ -42,33 +42,33 @@ void init() {
     tTraces->SetBranchAddress("SamIndex", SamIndex);
     tTraces->SetBranchAddress("IsSignal", &isSignal);
     tTraces->SetBranchAddress("IsBTF", &isBTF);
-		tTraces->SetBranchAddress("PeakPos", &PeakPos);
-		tTraces->SetBranchAddress("Npeaks", &Npeaks);
+    tTraces->SetBranchAddress("PeakPos", &PeakPos);
+    tTraces->SetBranchAddress("Npeaks", &Npeaks);
 
     numEntries = tTraces->GetEntries();
 }
 
 void end() {
-		fTraces->Close();
+    fTraces->Close();
 }
 
 void makeTemplate() {
 
-				init();
-//    std::vector<unsigned short> * t0 = 0;
-//    UShort_t Samples[1024], SamIndex[1024];
-//    Double_t Smooth[1024];
+    init();
+    //    std::vector<unsigned short> * t0 = 0;
+    //    UShort_t Samples[1024], SamIndex[1024];
+    //    Double_t Smooth[1024];
     hMinsJ = new TH1F("hMinsJ", "hMinsJ", 1024, 0, 1024);
     hMins = new TH1F("hMins", "hMins", 1024, 0, 4095);
     hTemplate = new TH1F("hTemplate", "hTemplate", 1000, 0, 999);
-		hMinsJ->SetDirectory(0);
-		hMins->SetDirectory(0);
-		hTemplate->SetDirectory(0);
-//    bool isSignal = 0; UInt_t isBTF = 0;
+    hMinsJ->SetDirectory(0);
+    hMins->SetDirectory(0);
+    hTemplate->SetDirectory(0);
+    //    bool isSignal = 0; UInt_t isBTF = 0;
 
     for (int j = 0; j < 1000; j++) templateVec[j] = 0;
     int numTraces[1000];
-	 	for (int j = 0; j < 1000; j++) numTraces[j] = 0;
+    for (int j = 0; j < 1000; j++) numTraces[j] = 0;
 
     //TFile * fTraces = TFile::Open("/Users/afranken/PADME/ECAL_TemplateFitting/EventScreen2.5ns.root");
     //TFile * fTraces = TFile::Open("/Users/afranken/PADME/ECAL_TemplateFitting/slimmed.root");
@@ -94,8 +94,8 @@ void makeTemplate() {
             int minj = -1;
 
             double baseline = 0;
-						for (int j = 0; j < 50; j++)
-										baseline += Samples[j]/50;
+            for (int j = 0; j < 50; j++)
+                baseline += Samples[j]/50;
 
             for (int j = 0; j < 1000; j++) {
                 if (Samples[j] < min) {
@@ -109,12 +109,12 @@ void makeTemplate() {
                         // Double_t temp = Smooth[j+(minj-200)];
                         //temp = (baseline - temp)/(baseline - minj);
                         // temp = (4096 - temp)/4096;
-												if (j < minj-30)
-																templateVec[j] += baseline;
-												else
-																templateVec[j] += Samples[j+(minj-400)];
-												numTraces[j]++;
-												//templateVec[j] += Samples[j];
+                        if (j < minj-30)
+                            templateVec[j] += baseline;
+                        else
+                            templateVec[j] += Samples[j+(minj-400)];
+                        numTraces[j]++;
+                        //templateVec[j] += Samples[j];
                         // templateVec[j] += temp;
                     }
                 }
@@ -144,9 +144,9 @@ void makeTemplate() {
     templateSpline->Draw("LCPSAME");
 
     std::cout << "Done building template." << std::endl;
-		
-		testTemplate();
-		test2Fits();
+
+    testTemplate();
+    test2Fits();
 }
 
 void testTemplate() {
@@ -171,7 +171,7 @@ void testTemplate() {
     for (int i = 0; i < numEntries; i++) {
 
         tTraces->GetEntry(i);
-				fitfunction->SetParameters(1, 0, 0, 1);
+        fitfunction->SetParameters(1, 0, 0, 1);
 
         if (isSignal&&isBTF) {
 
@@ -187,8 +187,8 @@ void testTemplate() {
                 }
             }
 
-						if (min > 3500)
-										continue;
+            if (min > 3500)
+                continue;
 
             Double_t x[1000], y[1000];
             for (int j = 0; j < 1000; j++) {
@@ -230,60 +230,60 @@ void testTemplate() {
 
     //fTraces->Close();
 
-//    TGraph * g = new TGraph(*hFits[541]);
-//
-//    TGraph * g2 = new TGraph(*hFits[541]);
-//
-//    TGraph * g3 = new TGraph(1000);
-//
-//    Double_t basex, basey;
-//    g->GetPoint(100, basex, basey);
-//    for (int i = 0; i < 1000; i++) {
-//        Double_t tempx, tempy;
-//        g->GetPoint(i, tempx, tempy);
-//        g->SetPoint(i, tempx, basey-tempy);
-//        if (i < 300)
-//            g3->SetPoint(i, tempx, basey-basey);
-//        if (i+300 < 1000)
-//            g3->SetPoint(i+300, tempx+300, basey-tempy);
-//    }
-//    g2->GetPoint(100, basex, basey);
-//    for (int i = 0; i < 1000; i++) {
-//        Double_t tempx, tempy;
-//        g2->GetPoint(i, tempx, tempy);
-//        g2->SetPoint(i, tempx, basey-tempy);
-//    }
-//    for (int i = 0; i < 1000; i++) {
-//        Double_t tempx, tempy, tempx2, tempy2;
-//        if (i+300 < 999) {
-//            g->GetPoint(i+400, tempx, tempy);
-//        }
-//        g2->GetPoint(i, tempx2, tempy2);
-//        if (i+300 < 999) {
-//            g->SetPoint(i+400, tempx, tempy+tempy2);
-//        }
-//    }
-//    for (int i = 0; i < 1000; i++) {
-//        Double_t tempx, tempy;
-//        g->GetPoint(i, tempx, tempy);
-//        g->SetPoint(i, tempx, 4000-tempy);
-//        g3->GetPoint(i, tempx, tempy);
-//        g3->SetPoint(i, tempx, 4000-tempy);
-//    }
-//    TCanvas * c2c = new TCanvas();
-//    g->Draw();
-//    // TF1 * testf2 = new TF1("fitf2", fitf2, 0, 1000, 5);
-//    TF1 * testf2 = new TF1("fitf2", fitf2, 0, 1000, 5);
-//    //testf2->SetParameters(1.4, 24, 1.0, 1.45162,307.07,1.037,-1866.81);
-//    // testf2->SetParameters(1.45162,307.07,-1866.81, 1.03783);
-//    // testf2->SetParLimits(0, 0, 3);
-//    // testf2->SetParLimits(1, 0, 1000);
-//    // testf2->SetParLimits(3, 0, 2);
-//    g->Fit("fitf2","R");
+    //    TGraph * g = new TGraph(*hFits[541]);
+    //
+    //    TGraph * g2 = new TGraph(*hFits[541]);
+    //
+    //    TGraph * g3 = new TGraph(1000);
+    //
+    //    Double_t basex, basey;
+    //    g->GetPoint(100, basex, basey);
+    //    for (int i = 0; i < 1000; i++) {
+    //        Double_t tempx, tempy;
+    //        g->GetPoint(i, tempx, tempy);
+    //        g->SetPoint(i, tempx, basey-tempy);
+    //        if (i < 300)
+    //            g3->SetPoint(i, tempx, basey-basey);
+    //        if (i+300 < 1000)
+    //            g3->SetPoint(i+300, tempx+300, basey-tempy);
+    //    }
+    //    g2->GetPoint(100, basex, basey);
+    //    for (int i = 0; i < 1000; i++) {
+    //        Double_t tempx, tempy;
+    //        g2->GetPoint(i, tempx, tempy);
+    //        g2->SetPoint(i, tempx, basey-tempy);
+    //    }
+    //    for (int i = 0; i < 1000; i++) {
+    //        Double_t tempx, tempy, tempx2, tempy2;
+    //        if (i+300 < 999) {
+    //            g->GetPoint(i+400, tempx, tempy);
+    //        }
+    //        g2->GetPoint(i, tempx2, tempy2);
+    //        if (i+300 < 999) {
+    //            g->SetPoint(i+400, tempx, tempy+tempy2);
+    //        }
+    //    }
+    //    for (int i = 0; i < 1000; i++) {
+    //        Double_t tempx, tempy;
+    //        g->GetPoint(i, tempx, tempy);
+    //        g->SetPoint(i, tempx, 4000-tempy);
+    //        g3->GetPoint(i, tempx, tempy);
+    //        g3->SetPoint(i, tempx, 4000-tempy);
+    //    }
+    //    TCanvas * c2c = new TCanvas();
+    //    g->Draw();
+    //    // TF1 * testf2 = new TF1("fitf2", fitf2, 0, 1000, 5);
+    //    TF1 * testf2 = new TF1("fitf2", fitf2, 0, 1000, 5);
+    //    //testf2->SetParameters(1.4, 24, 1.0, 1.45162,307.07,1.037,-1866.81);
+    //    // testf2->SetParameters(1.45162,307.07,-1866.81, 1.03783);
+    //    // testf2->SetParLimits(0, 0, 3);
+    //    // testf2->SetParLimits(1, 0, 1000);
+    //    // testf2->SetParLimits(3, 0, 2);
+    //    g->Fit("fitf2","R");
 
 
     TFile * outFile = TFile::Open("fits.root", "RECREATE");
-		gFile = outFile;
+    gFile = outFile;
     for (auto h : hFits)
         h->Write();
     hAmpl->Write();
@@ -307,10 +307,10 @@ void test2Fits() {
 
         tTraces->GetEntry(i);
 
-				if (Npeaks > 1)
-								fitfunction->SetParameters(1, 400-PeakPos->at(0), 1, 1, 400-PeakPos->at(1), 1, 0);
-				else
-								fitfunction->SetParameters(1, 0, 1, 1, 0, 1, 0);
+        if (Npeaks > 1)
+            fitfunction->SetParameters(1, 400-PeakPos->at(0), 1, 1, 400-PeakPos->at(1), 1, 0);
+        else
+            fitfunction->SetParameters(1, 0, 1, 1, 0, 1, 0);
 
         if (isSignal&&isBTF) {
 
@@ -326,8 +326,8 @@ void test2Fits() {
                 }
             }
 
-						if (min > 3500)
-										continue;
+            if (min > 3500)
+                continue;
 
             Double_t x[1000], y[1000];
             for (int j = 0; j < 1000; j++) {
@@ -352,11 +352,11 @@ void test2Fits() {
     }
 
     TFile * outFile = TFile::Open("fits2.root", "RECREATE");
-		gFile = outFile;
+    gFile = outFile;
     for (auto h : hFits)
         h->Write();
     outFile->Close();
 
-		end();
+    end();
     return;
 }
